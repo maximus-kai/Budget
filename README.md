42f93e9ecd0856956b9d29ab6176d8807bd6c11d
Maximus <tomiwadairo@gmail.com>
2024-03-17 11:19:00 +0100

    Setup files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Budget
-New Budget App(Refresher).
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      STARTING
+    </div>
+  );
+}
+
+export default App;
